2b1f2a96e29b7e0d1b639eeeda634fdacc6fb31d
iconanalytics <insights@iconanalytics.com>
2022-03-10 16:32:12 -0600

    second

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-23q(5$&oykgw79#an=d_jzmq_8g-ak=x$-$h0q54s7ccjw(mp2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
